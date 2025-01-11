9ffa02bb95a0ac39eea8da7cb1a87b56bc7ba5fb
havenousername <andrei.cristea997@gmail.com>
2025-01-11 12:49:16 +0100

    feat: migrate to v5 version of the strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,28 @@
+
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: [
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+        'Content-Type',
+        'authorization',
+        'Authorization',
+        'Access-Control-Request-Method',
+        'Access-Control-Request-Headers',
+        'Strapi-Response-Format',
+        'strapi-response-format',
+      ],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
