821e911a1ce6f428db81e041ff7a7abdea59a0ae
havenousername <andrei.cristea997@gmail.com>
2025-01-13 16:33:42 +0100

    feat: create user rdf-update call

--- src/extensions/users-permissions/content-types/user/lifecycles.js
+++ src/extensions/users-permissions/content-types/user/lifecycles.js
@@ -1,30 +1,48 @@
 const createUserStatistics = async (event) => {
-  const user = await strapi.db.query('plugin::users-permissions.user')
-    .findOne({ where: { email: event.params.data.email } });
-  const statistics = await strapi.db.query('api::user-statistics.user-statistics')
-    .create({ data: { user: user.id, visitedRegions: [], favouriteRegions: [] } });
-  strapi.log.info(`User statistics was connected
-      to ${event.model.attributes.id} with result ${JSON.stringify(statistics)}`);
-}
-
-
-const GRAPH_USER_ENDPOINT = process.env.WEBHOOK_ENDPOINT_URL + "/hook/user";
-
-
-const updateGraphRepresentation = async (event) => {
-  const result = await  fetch(`${GRAPH_USER_ENDPOINT}/${event.result.id}` , {
-    method:'PUT',
-    body: JSON.stringify(event.result),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  console.log(result);
-}
-
-const createGraphRepresentation = async (event) => {
   try {
-    const result = await fetch(GRAPH_USER_ENDPOINT, {
+    const statistics = await strapi.query('api::user-statistics.user-statistics')
+      .create({ data: { user: event.result.id, visitedRegions: [], favouriteRegions: [] } });
+    strapi.log.info(`User statistics was connected
+      to ${event.model.attributes.id} with result ${JSON.stringify(statistics)}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+const lifecycleState = {
+  nextGraphUpdate: true,
+  endpoint: process.env.WEBHOOK_ENDPOINT_URL + "/hook/user",
+}
+
+const updateGraphRepresentation = async (endpoint, event, state) => {
+  if (!state.nextGraphUpdate) {
+    state.nextGraphUpdate = true;
+    return;
+  }
+  try {
+    const result = await fetch(`${endpoint}/${event.result.id}` , {
+      method:'PUT',
+      body: JSON.stringify(event.result),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const resultBody = await result.json();
+    if (resultBody.error) {
+      strapi.log.warn(`Reaches the server ${endpoint} with error ${resultBody.error}`);
+    } else {
+      strapi.log.info(`Update graph representation with IRI ${resultBody.data}`);
+    }
+  } catch (error) {
+    strapi.log.warn(`Error happened while accessing ${endpoint} to update user with graph id ${event.result.graphId} graph representation with error ${error}`);
+  }
+}
+
+const createGraphRepresentation = async (endpoint, event, state) => {
+  try {
+    const result = await fetch(endpoint, {
       method: 'POST',
       body: JSON.stringify(event.result),
       headers: {
@@ -33,28 +51,27 @@ const createGraphRepresentation = async (event) => {
     });
     const resultBody = await result.json();
     if (resultBody.error) {
-      strapi.log.warn(`Reaches the server ${GRAPH_USER_ENDPOINT} with error ${resultBody.error}`);
+      strapi.log.warn(`Reaches the server ${endpoint} with error ${resultBody.error}`);
     } else {
       strapi.log.info(`Created graph representation with IRI ${resultBody.data}`);
+      state.nextGraphUpdate = false;
       await strapi.documents('plugin::users-permissions.user')
         .update({
           documentId: event.result.documentId,
           data: {graphId: resultBody.data}
-        })
+        });
     }
   } catch (error) {
-    strapi.log.warn(`Error happened while accessing ${GRAPH_USER_ENDPOINT} to create new user graph representation with error ${error}`);
+    strapi.log.warn(`Error happened while accessing ${endpoint} to create new user graph representation with error ${error}`);
   }
-
 }
 
 module.exports = {
   afterCreate(event) {
     createUserStatistics(event);
-    createGraphRepresentation(event);
+    createGraphRepresentation(lifecycleState.endpoint, event, lifecycleState);
   },
   afterUpdate(event) {
-    console.log(`User has been updated`, event);
-    updateGraphRepresentation(event);
+    updateGraphRepresentation(lifecycleState.endpoint, event, lifecycleState);
   }
 }
